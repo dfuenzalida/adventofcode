034ea92f13d1b4b93ccd0bf759d732f618bf5ee0
Denis Fuenzalida <denis.fuenzalida@gmail.com>
2018-01-14 20:46:32 -0800

    Day 03 part 2

--- advent-hs/src/Day03.hs
+++ advent-hs/src/Day03.hs
@@ -2,19 +2,19 @@ module Day03 where
 
 import Data.Map
 
-data Direction = LeftDir | RightDir | UpDir | DownDir
-               deriving (Eq, Ord, Show)
+shifts = fromList([("left", (-1,0)), ("right",(1,  0)),
+                   ("up",   ( 0,1)), ("down", (0, -1))])
 
-shifts = fromList([(LeftDir, (-1,0)), (RightDir,(1,  0)),
-                   (UpDir,   ( 0,1)), (DownDir, (0, -1))])
-
+moves :: Int -> [[Char]]
 moves n = firstPart ++ secondPart where
-  firstPart = Prelude.take n $ repeat $ if (odd n) then RightDir else LeftDir
-  secondPart = Prelude.take n $ repeat $ if (odd n) then UpDir else DownDir
+  firstPart = Prelude.take n $ repeat $ if (odd n) then "right" else "left"
+  secondPart = Prelude.take n $ repeat $ if (odd n) then "up" else "down"
 
+path :: Int -> [[Char]]
 path n = Prelude.take (n-1) allMoves where
   allMoves = concatMap moves [0..]
 
+distance :: Int -> Int
 distance n = firsts + seconds where
   firsts = abs $ foldl1 (+) $ Prelude.map fst steps
   seconds = abs $ foldl1 (+) $ Prelude.map snd steps
@@ -28,3 +28,25 @@ distance n = firsts + seconds where
 -- 31
 
 ----------------------------------------------------------------------
+
+allMoves = concatMap moves [1..]
+sumPairs (a,b) (c,d) = (a+c, b+d)
+
+computeGrid grid (x,y) = foldl1 (+) neighborVals where
+  neighborVals = Prelude.map (\p -> findWithDefault 0 p grid) $ neighbors (x,y)
+  neighbors (x,y) = zipWith sumPairs allDeltas (repeat (x,y))
+  allDeltas = Prelude.filter (\x -> x /= (0,0)) $ allPairs [-1, 0, 1] [-1, 0, 1]
+  allPairs xs ys = concatMap (\x->(Prelude.map (\y->(x, y))) ys) xs
+
+fillGrid grid (x,y) movesList limit =
+  if curr > limit then curr else fillGrid newGrid newCoord newMoves limit where
+    curr = computeGrid grid (x,y)
+    newGrid = insert (x,y) curr grid
+    newCoord = sumPairs (x,y) $ shifts ! (head movesList)
+    newMoves = tail movesList
+
+-- *Main Day03 Data.Map> fillGrid (fromList [((0,0), 1)]) (1,0) (Prelude.drop 1 allMoves) 80
+-- 122
+-- *Main Day03 Data.Map> fillGrid (fromList [((0,0), 1)]) (1,0) (Prelude.drop 1 allMoves) 999999
+-- 1009457
+
