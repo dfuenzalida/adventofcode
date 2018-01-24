b6e77294e24f0da9dba28a81a8fd24c12dcdefb3
Denis Fuenzalida <denis.fuenzalida@gmail.com>
2018-01-23 20:32:22 -0800

    Day 06 part 2

--- advent-hs/app/Main.hs
+++ advent-hs/app/Main.hs
@@ -1,6 +1,11 @@
 module Main where
 
-import Lib
+import Day05
+-- import Lib
+
+mainFunc = do
+  numLoops <- countLoops "resources/input05.txt" part2loop
+  putStrLn $ show numLoops
 
 main :: IO ()
-main = someFunc
+main = mainFunc
--- advent-hs/src/Day06.hs
+++ advent-hs/src/Day06.hs
@@ -1,6 +1,7 @@
 module Day06 where
 
 import qualified Data.Set as Set
+import qualified Data.Map as Map
 
 exampleBanks = [0, 2, 7, 0]
 
@@ -23,6 +24,24 @@ reallocate xs prevs i =
     maxIdx = snd $ head $ filter (\(x,_)->x == maxVal) $ zipWith (,) xs [0..]
     xs' = distribute (updateAt xs maxIdx 0) (1+maxIdx) maxVal
 
--- reallocate exampleBanks Set.empty 0 -> (5,5)
--- reallocate [4,10,4,1,8,4,9,14,5,1,14,15,0,15,3,5] Set.empty 0
+part1 :: [Integer] -> Integer
+part1 xs = reallocate xs Set.empty 0
 
+-- part1 exampleBanks -> (5,5)
+-- part1 [4,10,4,1,8,4,9,14,5,1,14,15,0,15,3,5]
+
+----------------------------------------------------------------------
+
+reallocate2 :: [Integer] -> Map.Map [Integer] Integer -> Integer -> Integer
+reallocate2 xs prevsMap i =
+  if Map.member xs prevsMap then i - (Map.findWithDefault 0 xs prevsMap) else
+    reallocate2 xs' (Map.insert xs i prevsMap) (1+i) where
+    maxVal = maximum xs
+    maxIdx = snd $ head $ filter (\(x,_)->x == maxVal) $ zipWith (,) xs [0..]
+    xs' = distribute (updateAt xs maxIdx 0) (1+maxIdx) maxVal
+
+part2 :: [Integer] -> Integer
+part2 xs = reallocate2 xs Map.empty 0
+
+-- part2 [2,4,1,2] -> 4
+-- part2 [4,10,4,1,8,4,9,14,5,1,14,15,0,15,3,5]
