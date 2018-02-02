d5e33ef5f6e3ee5d0210ed7b1e0c855c118850c8
Denis Fuenzalida <denis.fuenzalida@gmail.com>
2018-02-01 21:14:13 -0800

    Day 09 part 2

--- advent-hs/src/Day09.hs
+++ advent-hs/src/Day09.hs
@@ -5,12 +5,13 @@ dropAfter c xs = case xs of
   (y:ys) -> if y == c then dropAfter c (tail ys) else y : dropAfter c ys
   otherwise -> xs
 
+drop2 :: Bool -> String -> String
 drop2 xing xs = case (xs,xing) of
-  (('>':ys),_) -> drop2 False ys
-  (('<':ys),_) -> drop2 True ys
-  ((y:ys),True) -> drop2 True ys
-  ((y:ys),False) -> y: drop2 False ys
-  otherwise -> xs
+  (('>':ys),_)   -> drop2 False ys
+  (('<':ys),_)   -> drop2 True ys
+  ((y:ys),True)  -> drop2 True ys
+  ((y:ys),False) -> y : drop2 False ys
+  otherwise      -> xs
 
 clean :: String -> String
 clean s = drop2 False $ dropAfter '!' s
@@ -29,3 +30,16 @@ part1 = do
     fileContents <- readFile "resources/input09.txt"
     putStrLn $ show $ score (clean fileContents) 1 0
 
+----------------------------------------------------------------------
+
+countGarbage :: Integer -> Bool -> String -> Integer
+countGarbage n xing xs = case (xs,xing) of
+  (('>':ys),_)     -> countGarbage n False ys
+  (('<':ys),False) -> countGarbage n True ys
+  ((y:ys),True)    -> countGarbage (n+1) True ys
+  ((y:ys),False)   -> countGarbage n False ys
+  otherwise        -> n
+
+part2 = do
+    fileContents <- readFile "resources/input09.txt"
+    putStrLn $ show $ countGarbage 0 False $ (dropAfter '!' fileContents)
