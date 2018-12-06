98be43ed42efbc1cb11b8d70c0034a9201b5f728
Denis Fuenzalida <dfuenzal@amazon.com>
2018-12-06 12:19:21 -0800

    Day 03 part 2 fast

--- advent2018/src/advent2018/day03.clj
+++ advent2018/src/advent2018/day03.clj
@@ -1,6 +1,8 @@
 (ns advent2018.day03)
 
-(def example-input ["#1 @ 1,3: 4x4" "#2 @ 3,1: 4x4" "#3 @ 5,5: 2x2"])
+(def example-input ["#1 @ 1,3: 4x4"
+                    "#2 @ 3,1: 4x4"
+                    "#3 @ 5,5: 2x2"])
 
 (defn points-for [claim]
   (let [nums (->> claim
@@ -32,39 +34,51 @@
 
 ;; part 2 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defn delete-nth [xs n]
-  (concat (take n xs)
-          (drop (inc n) xs)))
+;; find the missing claim
 
-;; just the points in the outline of a rectangle
-(defn outline-for [claim]
+(defn points-and-claims-for [claim]
   (let [nums (->> claim
                   (re-seq #"#(.+) @ (.+),(.+): (.+)x(.+)")
                   first
                   rest)
         [claim-id left top width height] (map read-string nums)]
-    (set
-     (concat
-      (for [x (range left (+ left width))] [x top])
-      (for [x (range left (+ left width))] [x (+ -1 top height)])
-      (for [y (range top (+ top height))] [left y])
-      (for [y (range top (+ top height))] [(+ -1 left width) y])
-      ))))
+    (for [x (range left (+ left width))
+          y (range top (+ top height))]
+      {[x y] [claim-id]})))
 
-;; (outline-for "#1 @ 2,2: 2x2")
-;; (count (outline-for "#1 @ 0,0: 3x3")) => 8 ;; (3x3 grid minus the center point)
+(defn claims-for [claim]
+  (->> claim
+       (re-seq #"#(.+) @ .*")
+       first
+       rest
+       first
+       read-string))
 
-(def points-for-memo (memoize points-for))
+;; (claims-for (first example-input))
+
+;; map points to => [[x y] claim-id]
+;; group-by first
+;; filter kv pairs with values of more than 1 elem
+;; map second to find the values
+;; put all values into a set -> set of claims that overlap
+
+(defn overlapping-claims [input]
+  (->> input
+       (mapcat points-and-claims-for)
+       (apply merge-with into)
+       (map second)
+       (filter #(> (count %) 1))
+       (reduce into #{})))
+
+;; (overlapping-claims example-input)
 
 (defn part-2 [input]
-  (inc
-   (first
-    (filter some?
-            (for [n (range (count input))]
-              (let [most-points (set (mapcat points-for-memo (delete-nth input n)))
-                    claim-points (set (points-for-memo (nth input n)))]
-                (when-not (seq (filter claim-points most-points))
-                  n)))))))
+  (let [all-claims (set (map claims-for input))
+        ovr-claims (overlapping-claims input)]
+    (first
+     (for [claim all-claims
+           :when (not (ovr-claims claim))]
+       claim))))
 
 ;; (part-2 example-input)
 ;; (part-2 (read-input))
