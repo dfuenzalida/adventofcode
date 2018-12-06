759ea96219d7d6413b01578708482997b5cda921
Denis Fuenzalida <denis.fuenzalida@gmail.com>
2018-12-05 21:21:21 -0800

    Day 5 part 2

--- advent2018/src/advent2018/day05.clj
+++ advent2018/src/advent2018/day05.clj
@@ -7,17 +7,6 @@
 
 (def react2? (memoize react?))
 
-;; (react? \A \a)
-
-(defn reduce-polimer [input]
-  (loop [s (vec input), i 0]
-    (if (>= i (dec (count s)))
-      (apply str s)
-      (let [[a b] (subvec s i (+ i 2))]
-        (if (react2? a b)
-          (recur (into (subvec s 0 i) (subvec s (+ i 2))) (max 0 (+ -2 i)))
-          (recur s (inc i)))))))
-
 (defn reduce-polimer2 [^String input]
   (loop [s input, i 0]
     (if (>= i (dec (.length s)))
@@ -42,3 +31,18 @@
 ;; 10972
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn smallest-reduction [^String input]
+  (let [input   (s/trim input)
+        letters (->> input s/upper-case (map str) set)
+        lengths (for [letter letters]
+                  {letter
+                   (count
+                    (reduce-polimer2 (-> input
+                                         (.replaceAll letter "")
+                                         (.replaceAll (s/lower-case letter) ""))))})
+        len-map (reduce merge {} lengths)]
+    (->> len-map (map second) (apply min))))
+
+;; (smallest-reduction "dabAcCaCBAcCcaDA") => 4
+;; (time (smallest-reduction (s/trim (slurp "resources/day05.txt"))))
