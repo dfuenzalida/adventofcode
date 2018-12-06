4930eb49feb99b361dbdc1ee8010fd3b8d8db58c
Denis Fuenzalida <denis.fuenzalida@gmail.com>
2018-12-05 19:38:38 -0800

    Day 05 part 1 fast

--- advent2018/src/advent2018/day05.clj
+++ advent2018/src/advent2018/day05.clj
@@ -18,21 +18,27 @@
           (recur (into (subvec s 0 i) (subvec s (+ i 2))) (max 0 (+ -2 i)))
           (recur s (inc i)))))))
 
-(defn reduce-polimer2 [input]
+(defn reduce-polimer2 [^String input]
   (loop [s input, i 0]
     (if (>= i (dec (.length s)))
       s
       (let [a (.substring s i (inc i))
             b (.substring s (inc i) (+ i 2))]
         (if (react2? a b)
-          (recur (str (.substring s 0 i) (.substring s (+ i 2))) 0)
+          (recur (str (.substring s 0 i) (.substring s (+ i 2))) (max 0 (+ -2 i)))
           (recur s (inc i)))))))
 
-;; (reduce-polimer "aB")
-;; (reduce-polimer "abBA")
-;; (= "dabCBAcaDA" (reduce-polimer "dabAcCaCBAcCcaDA"))
-;; (= 10 (count (reduce-polimer "dabAcCaCBAcCcaDA")))
+;; (reduce-polimer2 "aB")
+;; (reduce-polimer2 "abBA")
+;; (= "dabCBAcaDA" (reduce-polimer2 "dabAcCaCBAcCcaDA"))
+;; (= 10 (count (reduce-polimer2 "dabAcCaCBAcCcaDA")))
 ;;
 ;;  (time (count (reduce-polimer (s/trim (slurp "resources/day05.txt")))))
 ;; "Elapsed time: 276899.992513 msecs" ;; AFTER POSITION OPTIMIZATION
 ;; 10972
+
+;; (time (count (reduce-polimer2 (s/trim (slurp "resources/day05.txt")))))
+;; "Elapsed time: 761.113723 msecs" ;; POSITION OPT + ^String type hint
+;; 10972
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
