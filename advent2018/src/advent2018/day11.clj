d67bf0997ecedda58024e10ab190e940529c1b8d
Denis Fuenzalida <denis.fuenzalida@gmail.com>
2018-12-21 21:07:39 -0800

    formatting, part 2 wip

--- advent2018/src/advent2018/day11.clj
+++ advent2018/src/advent2018/day11.clj
@@ -13,26 +13,31 @@
 (def grid-size 300)
 
 (defn power-grid [serial]
-  (into {}
-        (for [x (range grid-size)
-              y (range grid-size)]
-          {[x y] (power-level x y serial)})))
+  (into {} (for [x (range grid-size)
+                 y (range grid-size)]
+             {[x y] (power-level x y serial)})))
 
 (defn group-power [^long x ^long y grid]
-  (reduce +
-          (for [dx (range 3)
-                dy (range 3)]
-            (grid [(+ x dx) (+ y dy)]))))
+  (reduce + (for [dx (range 3)
+                  dy (range 3)]
+              (grid [(+ x dx) (+ y dy)]))))
 
 (defn max-group [[x1 y1 pow1] [x2 y2 pow2]]
   (if (> pow1 pow2) [x1 y1 pow1] [x2 y2 pow2]))
 
 (defn best-group [serial]
   (let [grid   (power-grid serial)
-        groups (for [x (range (- grid-size 2)) y (range (- grid-size 2))]
+        groups (for [x (range (- grid-size 2))
+                     y (range (- grid-size 2))]
                  [x y (group-power x y grid)])]
     (reduce max-group groups)))
 
 ;; (best-group 18)
 ;; (best-group 7347)
 
+;; part 2 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn bit-divide [n] ;; returns a seq of powers of 2 that sum N
+  (filter #(pos? (bit-and n %)) [256 128 64 32 16 8 4 2 1]))
+
+;; (bit-divide 300)
