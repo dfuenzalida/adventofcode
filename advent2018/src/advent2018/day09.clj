77fd79e0b04d383d415c7389e694e58b548f7620
Denis Fuenzalida <denis.fuenzalida@gmail.com>
2018-12-22 21:52:02 -0800

    Day 09 part 1 slow

--- advent2018/src/advent2018/day09.clj
+++ advent2018/src/advent2018/day09.clj
@@ -1,11 +1,17 @@
 (ns advent2018.day09)
 
-(defn insert-at [board index marble]
+(defn insert-at [board ^long index ^long marble] ;; returns [board index score]
   (let [index (inc (rem (dec index) (count board)))]
-    [(into [] (concat (take index board)
-                      [marble]
-                      (drop index board)))
-     index]))
+    (if (zero? (rem marble 23))
+      (let [to-remove (rem (+ index (count board) -9) (count board))
+            score     (+ marble (nth board to-remove))]
+        [(into [] (concat (take to-remove board)
+                          (drop (inc to-remove) board)))
+         to-remove score])
+      [(into [] (concat (take index board)
+                        [marble]
+                        (drop index board)))
+       index 0])))
 
 ;; (insert-at [0] 0 1)
 ;; (insert-at [0 1] 1 2)
@@ -17,12 +23,29 @@
 ;; marble - current marble
 ;; score-map - scores
 
-(defn iter-game [[board player index marble score-map]]
-  (let [[newboard newindex] (insert-at board (+ 2 index) marble)] ;; TODO case 23
+(defn iter-game [[board ^long player ^long index ^long marble score-map]]
+  (println player board)
+  (let [[newboard newindex score] (insert-at board (+ 2 index) marble)]
     [newboard
-     (rem (inc player) (count score-map))
+     (inc (rem player (count score-map)))
      newindex
      (inc marble)
-     score-map]))
+     (if (zero? score)
+       score-map
+       (update-in score-map [player] + score))]))
 
-;; (map (comp println first) (take 17 (iterate iter-game [[0] 1 0 1 (repeat 9 0)])))
+(defn zero-scores [n]
+  (into {} (map vector (range 1 (inc n)) (repeat n 0))))
+
+(defn game-max-score [num-players last-marble]
+  (->> (iterate iter-game [[0] 1 1 1 (zero-scores num-players)])
+       (drop last-marble) first last vals (apply max)))
+
+;; (game-max-score 9 25) => 32
+;; (game-max-score 10 1618) => 8317
+;; (game-max-score 13 7999) => 146373
+;; (game-max-score 17 1104) => 2764
+;; (game-max-score 21 6111) => 54718
+;; (game-max-score 30 5807) => 37305
+;; (time (game-max-score 459 72103)) => "Elapsed time: 1166720.191011 msecs"
+
