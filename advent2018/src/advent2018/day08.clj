e47c1d28b42a076c77679e90c070eb0d5c5f986c
Denis Fuenzalida <denis.fuenzalida@gmail.com>
2019-01-04 21:01:27 -0800

    Better Day 08 part 1

--- advent2018/src/advent2018/day08.clj
+++ advent2018/src/advent2018/day08.clj
@@ -3,23 +3,32 @@
 (def example-input
   [2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2])
 
-;; part 1 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(defn sum-metadata [xs num-nodes total]
-  (let [[num-children num-meta] (take 2 xs)]
-    (if (zero? num-nodes)
-      [xs total]
-      (if (zero? num-children)
-        (recur (drop (+ 2 num-meta) xs)
-               (dec num-nodes)
-               (reduce + total (take num-meta (drop 2 xs))))
-        (let [[xs total] (sum-metadata (drop 2 xs) num-children total)]
-          (let [meta (take num-meta xs)]
-            (recur (drop num-meta xs) (dec num-nodes) (reduce + total meta))))))))
-
 (defn read-input []
   (->> (slurp "resources/day08.txt") (format "[%s]") read-string))
 
-;; (last (sum-metadata example-input 1 0)) => 138
-;; (last (sum-metadata (read-input) 1 0))
+(defn parse [xs num-nodes] ;; returns [{:children [{...}] :meta [1 2 3]}]
+  (loop [xs xs, num-nodes num-nodes, nodes []] 
+    (if (zero? num-nodes)
+      [nodes xs]
+      (let [[[num-children num-meta] xs] (split-at 2 xs)
+            [children xs] (if (pos? num-children) (parse xs num-children) [[] xs])
+            [meta xs]     (split-at num-meta xs)]
+        (recur xs
+               (dec num-nodes)
+               (conj nodes {:children children :meta meta}))))))
 
+;; (clojure.pprint/pprint (->> (parse example-input 1) first))
+
+(defn part1 [input]
+  (let [tree (ffirst (parse input 1))]
+    (->> (tree-seq :children :children tree)
+         (mapcat :meta)
+         (reduce +))))
+
+;; (part1 example-input)
+;; (part1 (read-input))
+
+;; (clojure.pprint/pprint data)
+;; (clojure.walk/postwalk evaluate (first data))
+;; (->> (tree-seq :children :children (first data)) (mapcat :meta) (reduce +))
+                                   
