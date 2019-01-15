88ba1eaf575e791564ef2a20cc2b160796845244
Denis Fuenzalida <denis.fuenzalida@gmail.com>
2019-01-14 19:47:49 -0800

    Day 11 part 2 slow

--- advent2018/src/advent2018/day11.clj
+++ advent2018/src/advent2018/day11.clj
@@ -23,7 +23,7 @@
               (grid [(+ x dx) (+ y dy)]))))
 
 (defn max-group [[x1 y1 pow1] [x2 y2 pow2]]
-  (if (> pow1 pow2) [x1 y1 pow1] [x2 y2 pow2]))
+  (if (>= pow1 pow2) [x1 y1 pow1] [x2 y2 pow2]))
 
 (defn best-group [serial]
   (let [grid   (power-grid serial)
@@ -32,12 +32,58 @@
                  [x y (group-power x y grid)])]
     (reduce max-group groups)))
 
-;; (best-group 18)
-;; (best-group 7347)
+;; (time (best-group 18))
+;; (time (best-group 7347))
+;; (time (best-group2 7347))
 
 ;; part 2 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defn bit-divide [n] ;; returns a seq of powers of 2 that sum N
-  (filter #(pos? (bit-and n %)) [256 128 64 32 16 8 4 2 1]))
+(defn max-group2 [[x1 y1 size1 pow1] [x2 y2 size2 pow2]]
+  (let [best (if (> pow1 pow2)
+               [x1 y1 size1 pow1]
+               [x2 y2 size2 pow2])]
+    ;; (println "best so far:" best)
+    best))
 
-;; (bit-divide 300)
+(defn max-group2' [[x1 y1 size1 pow1] [x2 y2 size2 pow2]]
+  (let [best (if (> pow1 pow2)
+               [x1 y1 size1 pow1]
+               [x2 y2 size2 pow2])]
+    (println "best so far:" best)
+    best))
+
+(declare group-power2new-memo)
+
+(defn group-power2new [grid x y size]
+  (if (= 1 size)
+    (grid [x y])
+    (let [prev-power       (group-power2new-memo grid x y (dec size))
+          right-col-power  (reduce + (for [dy (range (- size 1))]
+                                       (grid [(+ x size -1) (+ y dy)])))
+          bottom-row-power (reduce + (for [dx (range (- size 1))]
+                                       (grid [(+ x dx) (+ y size -1)])))
+          bottom-right     (grid [(+ x size -1) (+ y size -1)])]
+      (+ prev-power right-col-power bottom-row-power bottom-right))))
+
+(def group-power2new-memo (memoize group-power2new))
+
+(defn best-of-size [grid size]
+  ;; (println size)
+  (let [all-tuples (for [x    (range 1 (- 300 size))
+                         y    (range 1 (- 300 size))]
+                     [x y size (group-power2new-memo grid x y size)])
+        best       (reduce max-group2 all-tuples)]
+    (println "best:" best)
+    best))
+
+(defn part2 [serial]
+  (let [grid       (power-grid serial)
+        all-tuples (for [size (range 1 33)]
+                     (best-of-size grid size))]
+    (reduce max-group2' all-tuples)))
+
+;; (time (part2 18))
+
+;; (time (part2 7347))
+;; "Elapsed time: 79467.896779 msecs"
+
